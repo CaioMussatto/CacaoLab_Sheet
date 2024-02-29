3b5106659d947f6868d4ecba30aca8cf0d93f56d
CaioMussatto <caio.mussatto@unesp.br>
2024-02-29 09:37:37 -0300

    First version of the code with some minors changes

--- planilha.py
+++ planilha.py
@@ -3,37 +3,37 @@ import datetime
 import holidays
 
 Pessoas = ["Victória",
-"Afonso",
-"Mateus",
-"Amanda",
-"Iago Silveira",
-"Giovana",
-"Fernando",
-"Ana",
-"Caio",
-"Maria Raquel",
-"Guilherme",
-"Isabeli",
-"Pedro"]
+           "Afonso",
+           "Mateus",
+           "Amanda",
+           "Iago Silveira",
+           "Giovana",
+           "Fernando",
+           "Ana",
+           "Caio",
+           "Maria Raquel",
+           "Guilherme",
+           "Isabeli",
+           "Pedro"]
 
 today = datetime.date.today() + datetime.timedelta(days=57)
 
 last_day = today.replace(month=12, day=31)
 
 data = []
-add_two_days = True 
+add_two_days = True
 
 while today <= last_day:
     if add_two_days:
-        today += datetime.timedelta(days=5)  
+        today += datetime.timedelta(days=5)
     else:
-        today += datetime.timedelta(days=2)  
+        today += datetime.timedelta(days=2)
     data.append(today)
     add_two_days = not add_two_days
 
 
 day_month = []
-br_holidays = holidays.BR(years = 2024, subdiv = "SP",)
+br_holidays = holidays.BR(years=2024, subdiv="SP",)
 
 for i in data:
     somas = []
@@ -45,7 +45,7 @@ for i in data:
 
 df = pd.DataFrame(
     {"Dias": day_month}
-) 
+)
 
 df["Pessoas"] = None
 df["Tipo"] = None
@@ -62,8 +62,8 @@ for holiday_date, holiday_name in sorted(br_holidays.items()):
 print(feriados)
 
 for i in range(len(df)):
-    pessoa_index = i % len(Pessoas)  
-    tipo = "Lab meeting" if i % 2 == 0 else "Journal Club"  
+    pessoa_index = i % len(Pessoas)
+    tipo = "Lab meeting" if i % 2 == 0 else "Journal Club"
     df.at[i, 'Pessoas'] = Pessoas[pessoa_index]
     df.at[i, 'Tipo'] = tipo
 
